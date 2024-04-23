39b83320634e01a68ad1e12bc5ee84faae7f6125
Abel201827 <166523741+Abel201827@users.noreply.github.com>
2024-04-23 22:40:30 +0000

    nuevo

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import ControlledCarousel from './index.html';
+
+function ControlledCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src="1.png" alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="2.png" alt="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="3.jpg" alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
