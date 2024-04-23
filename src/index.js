f715e2b7b84c8a91c0e7df7b0de1558bd921986b
Abel201827 <166523741+Abel201827@users.noreply.github.com>
2024-04-23 23:35:20 +0000

    vamos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import '.App.css';
+import { BrowserRouter as router, Switch, router, Router} from 'react-router-dom'
+import NavBar from './componentes/navegacion/navbar'
+import Inicio from './componentes/paginas/inicio'
+import Comparacion from './componentes/paginas/comparacion'
+import Ayuda from './componentes/paginas/ayuda'
 
 
 function App(){
@@ -6,7 +11,10 @@ function App(){
 
   return (
     <div className='App'>
-      <h1>NavBar</h1>
+
+      <router>
+        <NavBar/>
+      </router>
     </div>
   )
 }
