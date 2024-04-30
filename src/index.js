fdf2998b30f7908b719a43bccc0cc71031f8a67a
Abel201827 <166523741+Abel201827@users.noreply.github.com>
2024-04-30 14:53:21 +0000

    hola

--- src/index.js
+++ src/index.js
@@ -1,73 +1,40 @@
-import React from 'react';
-import { Container, Navbar, Nav, Carousel, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import { createRoot } from "react-dom/client";
+import Main from "./main";
+import Aside from "./aside";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Single from './single';
+import Basic from './layout/basic';
 
-function App() {
-  return (
-    <Container>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Tecno Alexander</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Comparación</Nav.Link>
-            <Nav.Link href="#link">Ayuda</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Basic />,
+        children: [
+            {
+                path: '',
+                element: <>
+                    <div className="col-md-8">
+                        <Main></Main>
+                    </div>
+                    <div className="col-md-4">
+                        <Aside></Aside>
+                    </div>
+                </>
+            },
+            {
+                path: "detalle",
+                element: <Single />,
+            },
+        ],
+    },
+]);
 
-      <div className="mt-4">
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/800x400?text=Imagen+1"
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/800x400?text=Imagen+2"
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/800x400?text=Imagen+3"
-              alt="Third slide"
-            />
-          </Carousel.Item>
-        </Carousel>
-      </div>
-
-      <div className="text-center mt-4">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum nisi id efficitur consectetur. Nulla facilisi. Aliquam erat volutpat.</p>
-      </div>
-
-      <div className="form-container">
-        <h2>Formulario</h2>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Correo electrónico</Form.Label>
-            <Form.Control type="email" placeholder="Ingrese su correo electrónico" />
-            <Form.Text className="text-muted">
-              No compartiremos su correo con nadie más.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" placeholder="Contraseña" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Enviar
-          </Button>
-        </Form>
-      </div>
-    </Container>
-  );
-}
-
-export default App;
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <RouterProvider router={router} />
+);
